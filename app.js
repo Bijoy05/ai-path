1cd13579dda4630c1e6040afe84a1cbf2ba8ac35
Bijoy Sen <bijoysen00@yahoo.com>
2025-04-22 23:09:20 +0200

    init repo5

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5501;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const word = "Hello World!";
+let word = "Hello World!";
 
 server.listen(port, (err) => {
     if (err) {
@@ -19,7 +19,8 @@ server.listen(port, (err) => {
 app.post('/calls', (req, res) => {
     res.status(200);
     console.log(req.body);
-    res.json({ 'connect': "NUMBER" })
+    const phoneNumber = "+46738514392"; // Replace with a real, validated number
+    res.status(200).json({ connect: phoneNumber });
     word = "I love fat women"
     res.end();
 });
