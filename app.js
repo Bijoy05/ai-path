a3eaf78cb45399c7c860964e38898df308dd9a2e
Bijoy Sen <bijoysen00@yahoo.com>
2025-04-22 23:31:30 +0200

    init repo9

--- app.js
+++ app.js
@@ -16,19 +16,28 @@ server.listen(port, (err) => {
     console.log('Server running on port ' + port);
 });
 
-app.post('/calls', (req, res) => {
-    console.log(req.body);
-    const phoneNumber = "+46738514392"; // Replace with a real, validated number
-    res.status(200).json({ connect: phoneNumber });
-    word = "kai is gae"
-    res.end();
-});
+// app.post('/calls', (req, res) => {
+//     console.log(req.body);
+//     const phoneNumber = "+46738514392"; // Replace with a real, validated number
+//     res.status(200).json({ connect: phoneNumber });
+//     word = "kai is gae"
+//     res.end();
+// });
 
 app.post('/call1', (req, res) => {
     const phoneNumber = "+46123456789"; // temporary test number
     console.log("Received request from 47elks:", req.body);
     console.log("Responding with number:", phoneNumber);
     res.status(200).json({ connect: phoneNumber });
+});
+
+app.post('/calls', (req, res) => {
+  const response = {
+    play: "https://file-examples.com/storage/feeed4f6296807c3196e058/2017/11/file_example_MP3_700KB.mp3",
+    skippable: false
+  };
+  word = "kai is gae";
+  res.status(200).json(response);
 });
 
 app.get('/', (req, res) => {
