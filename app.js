8c7af4b1b7016c44d00263fc3d181493c839587b
Bijoy Sen <bijoysen00@yahoo.com>
2025-04-23 13:37:24 +0200

    call + sms10

--- app.js
+++ app.js
@@ -75,35 +75,28 @@ app.post('/calls', (req, res) => {
 //   });
 
 
-app.post('/sms', async (req, res) => {
-//   const callerNumber = req.body.direction === "incoming" ? req.body.from : null;
+app.post('/sms', (req, res) => {
+  const username = "ueda2920ccc39c93e7b25615f78d7a3e5";
+  const password = "B8E8B3A2B7DCE63644F8758324A6803";
+  const auth  = Buffer.from(username + ":" + password).toString("base64");
 
-  if (True) {
-    const username = "ueda2920ccc39c93e7b25615f78d7a3e5";
-    const password = "B8E8B3A2B7DCE63644F8758324A6803";
-    const auth = Buffer.from(username + ":" + password).toString("base64");
+  let data = {
+    from: "NodeElk",
+    to: "+46734866902",
+    message: "Thank you for calling. Please visit https://www.google.com"
+  };
 
-    let data = {
-      from: "+46766868561",
-      to: "+46734866902",
-      message: "Thank you for calling. Please visit https://www.google.com"
-    };
+  data = new URLSearchParams(data);
+  data = data.toString();
 
-    data = new URLSearchParams(data).toString();
-
-    try {
-      await fetch("https://api.46elks.com/a1/sms", {
-        method: "post",
-        body: data,
-        headers: {
-          "Authorization": "Basic " + auth,
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
-      });
-    } catch (err) {
-      console.log("SMS sending failed:", err);
-    }
-  }
+  fetch("https://api.46elks.com/a1/sms", {
+    method: "post",
+    body: data,
+    headers: { "Authorization": "Basic "  + auth }
+  })
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.log(err));
 
   res.status(200).end();
 });
